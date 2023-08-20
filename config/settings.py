b776a11f4d5f8569c701f658642c0e044568e4c8
Your Name <firdavsef700@gmail.com>
2023-08-20 16:50:36 +0500

    deploy-psycopg2

--- config/settings.py
+++ config/settings.py
@@ -65,24 +65,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# ================================================== PostgresSQL =======================================================
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "fath_recuter",
-        'USER': "postgres",
-        'PASSWORD': "1",
-        'HOST': 'localhost',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# ================================================== PostgresSQL =======================================================
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': "fath_recuter",
+#         'USER': "postgres",
+#         'PASSWORD': "1",
+#         'HOST': 'localhost',
+#         'PORT': 5432,
+#     }
+# }
 
 
 
