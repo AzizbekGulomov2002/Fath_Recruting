e03e9def238c69ce6b2fa7c82f1e03660b36682d
Your Name <firdavsef700@gmail.com>
2023-08-20 18:01:42 +0500

    deploy

--- config/settings.py
+++ config/settings.py
@@ -138,14 +138,15 @@ MODELTRANSLATION_TRANSLATION_FILES = (
 )
 
 import os
+
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 
 
 # Default primary key field type
